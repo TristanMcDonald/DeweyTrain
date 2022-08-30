4436504a2e67239c120475590287e0cb29dfe9ce
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-08-30 15:10:37 +0200

    Added the ReplacingBooksPage

---
+++ Views/ReplacingBooksPage.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeweyTrain.Views
+{
+    /// <summary>
+    /// Interaction logic for ReplacingBooksPage.xaml
+    /// </summary>
+    /// 
+    public class Item
+    {
+        public string Name { get; set; }
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public partial class ReplacingBooksPage : Page
+    {
+
+        private Point _dragStartPoint;
+
+        private T FindVisualParent<T>(DependencyObject child)
+            where T : DependencyObject
+        {
+            var parentObject = VisualTreeHelper.GetParent(child);
+            if (parentObject == null)
+                return null;
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            return FindVisualParent<T>(parentObject);
+        }
+
+        private IList<Item> _items = new ObservableCollection<Item>();
+
+        //List which will contain the randomly generated call numbers.
+        //List<double> List = new List<double>();
+        
+        public ReplacingBooksPage()
+        {
+            InitializeComponent();
+
+            //Calling the addListItems method to add the randomly generated
+            //call numbers to the list instantiated above.
+            addListItems();
+
+            //Iterating through each item in the list and adding them to the listbox.
+            //foreach(var item in _items)
+            //{
+            //    listbox1.Items.Add(item);
+            //}
+
+            listbox1.DisplayMemberPath = "Name";
+            listbox1.ItemsSource = _items;
+
+            listbox1.PreviewMouseMove += ListBox_PreviewMouseMove;
+
+            var style = new Style(typeof(ListBoxItem));
+            style.Setters.Add(new Setter(ListBoxItem.AllowDropProperty, true));
+            style.Setters.Add(
+                new EventSetter(
+                    ListBoxItem.PreviewMouseLeftButtonDownEvent,
+                    new MouseButtonEventHandler(ListBoxItem_PreviewMouseLeftButtonDown)));
+            style.Setters.Add(
+                    new EventSetter(
+                        ListBoxItem.DropEvent,
+                        new DragEventHandler(ListBoxItem_Drop)));
+            listbox1.ItemContainerStyle = style;
+
+
+        }
+
+        public void addListItems()
+        {
+            //Random object for use with randomly generating call numbers.
+            Random rd = new Random();
+
+            //For loop to generate random numbers 10 times.
+            for(int i = 0; i < 10; i++)
+            {
+                double rand_num = rd.Next(0, 999);
+
+                string randSrtNum = rand_num.ToString();
+
+                _items.Add(new Item(randSrtNum));
+
+            }
+        }
+
+
+        private void ListBox_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(null);
+            Vector diff = _dragStartPoint - point;
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                var lb = sender as ListBox;
+                var lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
+                if (lbi != null)
+                {
+                    DragDrop.DoDragDrop(lbi, lbi.DataContext, DragDropEffects.Move);
+                }
+            }
+        }
+        private void ListBoxItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _dragStartPoint = e.GetPosition(null);
+        }
+
+        private void ListBoxItem_Drop(object sender, DragEventArgs e)
+        {
+            if (sender is ListBoxItem)
+            {
+                var source = e.Data.GetData(typeof(Item)) as Item;
+                var target = ((ListBoxItem)(sender)).DataContext as Item;
+
+                int sourceIndex = listbox1.Items.IndexOf(source);
+                int targetIndex = listbox1.Items.IndexOf(target);
+
+                Move(source, sourceIndex, targetIndex);
+            }
+        }
+
+        private void Move(Item source, int sourceIndex, int targetIndex)
+        {
+            if (sourceIndex < targetIndex)
+            {
+                _items.Insert(targetIndex + 1, source);
+                _items.RemoveAt(sourceIndex);
+            }
+            else
+            {
+                int removeIndex = sourceIndex + 1;
+                if (_items.Count + 1 > removeIndex)
+                {
+                    _items.Insert(targetIndex, source);
+                    _items.RemoveAt(removeIndex);
+                }
+            }
+        }
+
+    }
+}
