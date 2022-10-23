f94fcb21271d3150467dc80b70de73ef646f5c05
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-10-23 21:29:12 +0200

    Implemented functionality for the application to check the users answer and determine whether it is correct or incorrect as well as implemented error handling for buttons and null returns

--- Views/IdentifyingAreasPage.xaml.cs
+++ Views/IdentifyingAreasPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -39,16 +40,19 @@ namespace DeweyTrain.Views
         };
 
         //The dictionary that will store all the users answers.
-        private Dictionary<string, string> questions = new Dictionary<string, string>();
+        private Dictionary<string, string> qADict = new Dictionary<string, string>();
+        private Dictionary<string, string> userAnswer = new Dictionary<string, string>();
 
         //The ArrayLists that will display the call numbers and descriptions in the list boxes.
         public ArrayList _callNumbers = new ArrayList();
         public ArrayList _descriptions = new ArrayList();
 
+        public ArrayList _callNumbersAnswer = new ArrayList();
+        public ArrayList _descriptionsAnswer = new ArrayList();
+
         public IdentifyingAreasPage()
         {
             InitializeComponent();
-
             generateListItems();
 
             QuestionListBox1.ItemsSource = _callNumbers;
@@ -62,11 +66,26 @@ namespace DeweyTrain.Views
             int max = 4;
             int now;
             //Retrieving random call numbers and their descriptions from the dictionary of call numbers and descriptions.
+            //The following code refrences StackOverFlow:
+            //Author: Tim
+            //https://stackoverflow.com/questions/23543128/take-a-random-item-from-one-list-and-add-it-to-another-list
             for (int i = 0; i < max; i++)
             {
                 now = r.Next(0, questionsAnswer.Count);
                 _callNumbers.Add(questionsAnswer.ElementAt(now).Key.ToString());
                 _descriptions.Add(questionsAnswer.ElementAt(now).Value.ToString());
+                qADict.Add(questionsAnswer.ElementAt(now).Key.ToString(), questionsAnswer.ElementAt(now).Value.ToString());
+                questionsAnswer.Remove(questionsAnswer.ElementAt(now).Key); 
+            }
+
+            int now2;
+            //Retrieving extra random descriptions from the dictionary of call numbers and descriptions.
+            for (int i = 0; i < 3; i++)
+            {
+                now2 = r.Next(0, questionsAnswer.Count);
+
+                _descriptions.Add(questionsAnswer.ElementAt(now2).Value.ToString());
+                questionsAnswer.Remove(questionsAnswer.ElementAt(now2).Key);
             }
 
         }
@@ -74,23 +93,52 @@ namespace DeweyTrain.Views
         //Check the user's answer.
         private void checkAnswerBtn_Click(object sender, RoutedEventArgs e)
         {
-
-            bool isUserCorrect = true;
-
-            //Checking each key value pair in the users answer
-            //to ensure that the right key is paired with the right value
-            //(the right call number is paired with the right description).
-            foreach (var qKvp in questions)
+            //If the answerslist box does not contain any items display an error message.
+            if (AnswerListBox1.Items.Count<1 || AnswerListBox2.Items.Count < 1)
             {
-                var descriptionAnswer = from qaKvp in questionsAnswer
-                                        where qaKvp.Key == qKvp.Key
-                                        select qaKvp.Value;
+                errorEmptyAnswerLabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                errorEmptyAnswerLabel.Visibility = Visibility.Hidden;
 
-                if (qKvp.Value != descriptionAnswer.ToString())
+                //Clear out any old stored values in the user's Answers dictionary.
+                userAnswer.Clear();
+
+                //Getting the items form the users Answer list boxes and adding them to the userAnswers Dictionary.
+                for (int i = 0; i < AnswerListBox1.Items.Count; i++)
                 {
-                    isUserCorrect = false;
+                    userAnswer.Add(AnswerListBox1.Items.GetItemAt(i).ToString(), AnswerListBox2.Items.GetItemAt(i).ToString());
                 }
 
+                bool isUserCorrect = false;
+
+                //The following code refrences StackOverFlow:
+                //Author: Jon Skeet
+                //https://stackoverflow.com/questions/6753196/comparing-two-dictionaries-in-c-sharp
+
+                //Sorting the dictionary that stores the users answers to the question as well as
+                //the dictionary that stores the actual answers to the question and then comparing
+                //their key value pairs with one another.
+                var orderedqADict = qADict.OrderBy(kvp => kvp.Key);
+                var orderedUserAnswer = userAnswer.OrderBy(kvp => kvp.Key);
+
+                if (orderedqADict.SequenceEqual(orderedUserAnswer))
+                {
+                    isUserCorrect = true;
+                }
+
+                if (isUserCorrect.Equals(true))
+                {
+                    correctLabel.Visibility = Visibility.Visible;
+                    incorrectLabel.Visibility = Visibility.Collapsed;
+                    MessageBox.Show("You have received 5 points", "CONGRATULATIONS", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    incorrectLabel.Visibility = Visibility.Visible;
+                    correctLabel.Visibility = Visibility.Collapsed;
+                }
             }
         }
 
@@ -112,60 +160,109 @@ namespace DeweyTrain.Views
         }
 
         //Declarations for variables used in button Add and Remove clicked events.
+        //The following button click events refrences c-sharpcorner.com:
+        //Author: Mahesh Chand
+        //https://www.c-sharpcorner.com/uploadfile/mahesh/transferring-data-from-one-listbox-to-another-in-wpf/
+
         public string? currentItemText;
         int currentItemIndex;
 
         private void AddButton1_Click(object sender, RoutedEventArgs e)
         {
-            // Find the right item and it's value and index  
-            currentItemText = QuestionListBox1.SelectedValue.ToString();
-            currentItemIndex = QuestionListBox1.SelectedIndex;
-            AnswerListBox1.Items.Add(currentItemText);
-            if (_callNumbers != null)
+            //Error handling (Null reference exception)
+            //(https://www.tutorialkart.com/c-sharp-tutorial/c-sharp-nullreferenceexception/#:~:text=How%20to%20Solve%20or%20Handle%20NullReferenceException%3F%201%20Using,is%20not%20null.%20Program.cs%20...%203%20Conclusion%20)
+            if (QuestionListBox1.SelectedItem == null)
             {
-                _callNumbers.RemoveAt(currentItemIndex);
+                errorSelectItemAddLabel.Visibility = Visibility.Visible;
             }
-            // Refresh data binding  
-            ApplyCallNumberDataBinding();           
+            else
+            {
+                errorSelectItemAddLabel.Visibility = Visibility.Hidden;
+                // Find the right item and it's value and index  
+                currentItemText = QuestionListBox1.SelectedValue.ToString();
+                currentItemIndex = QuestionListBox1.SelectedIndex;
+                AnswerListBox1.Items.Add(currentItemText);
+                if (_callNumbers != null)
+                {
+                    _callNumbers.RemoveAt(currentItemIndex);
+                }
+                // Refresh data binding  
+                ApplyCallNumberDataBinding();
+            }      
         }
 
         private void RemoveButton1_Click(object sender, RoutedEventArgs e)
         {
-            // Find the right item and it's value and index  
-            currentItemText = AnswerListBox1.SelectedValue.ToString();
-            currentItemIndex = AnswerListBox1.SelectedIndex;
-            // Add RightListBox item to the ArrayList  
-            _callNumbers.Add(currentItemText);
-            AnswerListBox1.Items.RemoveAt(AnswerListBox1.Items.IndexOf(AnswerListBox1.SelectedItem));
-            // Refresh data binding  
-            ApplyCallNumberDataBinding();
+            //Error handling (Null reference exception)
+            //(https://www.tutorialkart.com/c-sharp-tutorial/c-sharp-nullreferenceexception/#:~:text=How%20to%20Solve%20or%20Handle%20NullReferenceException%3F%201%20Using,is%20not%20null.%20Program.cs%20...%203%20Conclusion%20)
+            if (AnswerListBox1.SelectedItem == null)
+            {
+                errorSelectItemRemoveLabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                errorSelectItemRemoveLabel.Visibility = Visibility.Hidden;
+                // Find the right item and it's value and index  
+                currentItemText = AnswerListBox1.SelectedValue.ToString();
+                currentItemIndex = AnswerListBox1.SelectedIndex;
+                // Add RightListBox item to the ArrayList  
+                _callNumbers.Add(currentItemText);
+                AnswerListBox1.Items.RemoveAt(AnswerListBox1.Items.IndexOf(AnswerListBox1.SelectedItem));
+                // Refresh data binding  
+                ApplyCallNumberDataBinding();
+            }               
         }
 
         private void AddButton2_Click(object sender, RoutedEventArgs e)
         {
-            // Find the right item and it's value and index  
-            currentItemText = QuestionListBox2.SelectedValue.ToString();
-            currentItemIndex = QuestionListBox2.SelectedIndex;
-            AnswerListBox2.Items.Add(currentItemText);
-            if (_descriptions != null)
+            //Error handling (Null reference exception)
+            //(https://www.tutorialkart.com/c-sharp-tutorial/c-sharp-nullreferenceexception/#:~:text=How%20to%20Solve%20or%20Handle%20NullReferenceException%3F%201%20Using,is%20not%20null.%20Program.cs%20...%203%20Conclusion%20)
+            if (QuestionListBox2.SelectedItem == null)
             {
-                _descriptions.RemoveAt(currentItemIndex);
+                errorSelectItemAddLabel.Visibility = Visibility.Visible;
             }
-            // Refresh data binding  
-            ApplyDescriptionDataBinding();
+            else
+            {
+                errorSelectItemAddLabel.Visibility = Visibility.Hidden;
+                // Find the right item and it's value and index  
+                currentItemText = QuestionListBox2.SelectedValue.ToString();
+                currentItemIndex = QuestionListBox2.SelectedIndex;
+                AnswerListBox2.Items.Add(currentItemText);
+                if (_descriptions != null)
+                {
+                    _descriptions.RemoveAt(currentItemIndex);
+                }
+                // Refresh data binding  
+                ApplyDescriptionDataBinding();
+            } 
         }
     
 
         private void RemoveButton2_Click(object sender, RoutedEventArgs e)
         {
-            // Find the right item and it's value and index  
-            currentItemText = AnswerListBox2.SelectedValue.ToString();
-            currentItemIndex = AnswerListBox2.SelectedIndex;
-            // Add RightListBox item to the ArrayList  
-            _descriptions.Add(currentItemText);
-            AnswerListBox2.Items.RemoveAt(AnswerListBox2.Items.IndexOf(AnswerListBox2.SelectedItem));
-            // Refresh data binding  
-            ApplyDescriptionDataBinding();
+            //Error handling (Null reference exception)
+            //(https://www.tutorialkart.com/c-sharp-tutorial/c-sharp-nullreferenceexception/#:~:text=How%20to%20Solve%20or%20Handle%20NullReferenceException%3F%201%20Using,is%20not%20null.%20Program.cs%20...%203%20Conclusion%20)
+            if (AnswerListBox2.SelectedItem == null)
+            {
+                errorSelectItemRemoveLabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                errorSelectItemRemoveLabel.Visibility = Visibility.Hidden;
+                // Find the right item and it's value and index  
+                currentItemText = AnswerListBox2.SelectedValue.ToString();
+                currentItemIndex = AnswerListBox2.SelectedIndex;
+                // Add RightListBox item to the ArrayList  
+                _descriptions.Add(currentItemText);
+                AnswerListBox2.Items.RemoveAt(AnswerListBox2.Items.IndexOf(AnswerListBox2.SelectedItem));
+                // Refresh data binding  
+                ApplyDescriptionDataBinding();
+            }      
+        }
+
+        private void restartAreasBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
